fed1d3e88b20557bd25460f44c1bf8b1f828d5b3
Shantanu2705 <shantanubiswas951@gmail.com>
2026-07-20 21:58:10 +0530

    Remove debug banner

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -11,10 +11,6 @@ export default async function PublicLayout({
 
   return (
     <div className="min-h-screen flex flex-col relative">
-      {/* GIANT RED DEBUG BANNER */}
-      <div className="bg-red-600 text-white font-bold text-center py-2 text-xl z-50 shadow-lg shadow-red-500/50 sticky top-0">
-        DEBUG MODE V2 - NEW DEPLOYMENT IS ACTIVE
-      </div>
       <Navbar settings={settings} />
       <main className="flex-1">
         {children}
